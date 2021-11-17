6f065d2a063d96d020328cd27f81be94e7ec2038
Adomas Sliuzinskas <adomas@aion.dk>
2021-11-17 15:27:19 +0100

    Attempt to have an end-to-end test with all backend systems in place

--- cypress/integration/voter_flow.spec.js
+++ cypress/integration/voter_flow.spec.js
@@ -1,9 +1,12 @@
 describe('Voter', () => {
+  const e2e = Cypress.env('END_TO_END');
+
   beforeEach(() => {
     cy.viewport('iphone-5');
   });
 
   it('casts a vote', () => {
+    let otpDigits = null;
     const pageTransitionDuration = 300;
     const startupScreenDuration = 2000;
 
@@ -28,23 +31,55 @@ describe('Voter', () => {
     cy.contains('app-request-access-code ion-button', 'Next').click();
     cy.wait(pageTransitionDuration);
     cy.contains('app-access-code', 'Enter Access Code').should('be.visible');
+    if (e2e) {
+      cy.wait(1000); // Wait for email to be sent
+      cy.request('http://localhost:1080/messages').then((response) => {
+        const messages = response.body;
+        const lastMessageId = messages[messages.length - 1].id;
+        cy.request(`http://localhost:1080/messages/${lastMessageId}.plain`).then((response) => {
+          const message = response.body;
+          const otpPattern = /\d{5}/g;
+          otpDigits = otpPattern.exec(message)[0].split('');
 
-    cy.get('app-access-code input[tabindex="1"]').type('1');
-    cy.get('app-access-code input[tabindex="2"]').type('2');
-    cy.get('app-access-code input[tabindex="3"]').type('3');
-    cy.get('app-access-code input[tabindex="4"]').type('4');
-    cy.get('app-access-code input[tabindex="5"]').type('5');
+          cy.get('app-access-code input[tabindex="1"]').type(otpDigits[0]);
+          cy.get('app-access-code input[tabindex="2"]').type(otpDigits[1]);
+          cy.get('app-access-code input[tabindex="3"]').type(otpDigits[2]);
+          cy.get('app-access-code input[tabindex="4"]').type(otpDigits[3]);
+          cy.get('app-access-code input[tabindex="5"]').type(otpDigits[4]);
 
-    cy.contains('app-access-code ion-button', 'Next').click();
-    cy.wait(pageTransitionDuration);
-    cy.contains('app-before-you-finish', 'Before You Finish').should('be.visible');
+          cy.contains('app-access-code ion-button', 'Next').click();
+          cy.wait(pageTransitionDuration);
+          cy.contains('app-before-you-finish', 'Before You Finish').should('be.visible');
 
-    cy.contains('app-before-you-finish ion-button', 'Next').click();
-    cy.wait(pageTransitionDuration);
-    cy.contains('app-ballot-fingerprint', 'Your Ballot Has Been Sealed and Is Ready To Cast').should('be.visible');
+          cy.contains('app-before-you-finish ion-button', 'Next').click();
+          cy.wait(pageTransitionDuration);
+          cy.contains('app-ballot-fingerprint', 'Your Ballot Has Been Sealed and Is Ready To Cast').should('be.visible');
 
-    cy.contains('app-ballot-fingerprint ion-button', 'Cast Ballot Now').click();
-    cy.wait(pageTransitionDuration);
-    cy.contains('app-sending-confirmation', 'Congrats! Your ballot has been cast!').should('be.visible');
+          cy.contains('app-ballot-fingerprint ion-button', 'Cast Ballot Now').click();
+          cy.wait(pageTransitionDuration);
+          cy.contains('app-sending-confirmation', 'Congrats! Your ballot has been cast!').should('be.visible');
+        });
+      });
+    } else {
+      otpDigits = '12345'.split('');
+
+      cy.get('app-access-code input[tabindex="1"]').type(otpDigits[0]);
+      cy.get('app-access-code input[tabindex="2"]').type(otpDigits[1]);
+      cy.get('app-access-code input[tabindex="3"]').type(otpDigits[2]);
+      cy.get('app-access-code input[tabindex="4"]').type(otpDigits[3]);
+      cy.get('app-access-code input[tabindex="5"]').type(otpDigits[4]);
+
+      cy.contains('app-access-code ion-button', 'Next').click();
+      cy.wait(pageTransitionDuration);
+      cy.contains('app-before-you-finish', 'Before You Finish').should('be.visible');
+
+      cy.contains('app-before-you-finish ion-button', 'Next').click();
+      cy.wait(pageTransitionDuration);
+      cy.contains('app-ballot-fingerprint', 'Your Ballot Has Been Sealed and Is Ready To Cast').should('be.visible');
+
+      cy.contains('app-ballot-fingerprint ion-button', 'Cast Ballot Now').click();
+      cy.wait(pageTransitionDuration);
+      cy.contains('app-sending-confirmation', 'Congrats! Your ballot has been cast!').should('be.visible');
+    }
   });
 });
